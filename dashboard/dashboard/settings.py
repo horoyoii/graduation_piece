ceee1daf269ce57a080ced80de0d8f879e67b79b
horoyoii <demonic3540@naver.com>
2020-04-28 00:54:09 +0900

    Add docker-compose

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
 CACHES = {  
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1", # 1번 DB
+        "LOCATION": "redis://redis:6379/1", # 1번 DB
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
